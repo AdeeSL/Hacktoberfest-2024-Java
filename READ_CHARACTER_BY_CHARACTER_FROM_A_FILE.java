3adf47b898113a67a91e194c7d0ae13fae218ef7
Worker97 <72203247+Worker97@users.noreply.github.com>
2023-10-27 14:57:30 +0530

    Create READ_CHARACTER_BY_CHARACTER_FROM_A_FILE.java

---
+++ READ_CHARACTER_BY_CHARACTER_FROM_A_FILE.java
@@ -0,0 +1,13 @@
+# to read file character by character
+file = open('YourTextFile.txt', 'r')
+ 
+while 1:
+     
+    # read by character
+    char = file.read(1)          
+    if not char: 
+        break
+         
+    print(char)
+ 
+file.close()
